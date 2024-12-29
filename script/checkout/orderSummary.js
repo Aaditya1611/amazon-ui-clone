5b871540039c2d57aeb2827dcf3601b4bc2a72df
Aaditya1611 <aadityaraj1611@gmail.com>
2024-12-29 18:10:31 +0530

    make payment service interactive

--- script/checkout/orderSummary.js
+++ script/checkout/orderSummary.js
@@ -3,6 +3,7 @@ import { products , getProduct } from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import { deliveryOptions , getDeliverOption } from '../../data/deliveryOptions.js';
+import { renderPaymentSummary } from './paymentSummary.js';
 
 export function renderOrderSummary() {
 
@@ -108,9 +109,11 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
     link.addEventListener('click', () => {
         const productId = link.dataset.productId;
         removeFromCart(productId);
-
+        
         const container = document.querySelector(`.js-cart-item-container-${productId}`)
         container.remove();
+
+        renderPaymentSummary();
     })
 });
 
@@ -119,6 +122,7 @@ document.querySelectorAll('.js-delivery-option').forEach((element) => {
         const {productId , deliveryOptionId} = element.dataset;
         updateDeliveryOption(productId , deliveryOptionId)
         renderOrderSummary();
+        renderPaymentSummary();
     })
 });
 
