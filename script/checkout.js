57fd7a94d1ff02618276fe5ed539a6a03aea3c85
aadityaraj1611 <aadityaraj1611@gmail.com>
2025-01-02 01:44:20 +0530

    convert from promises to async await

--- script/checkout.js
+++ script/checkout.js
@@ -5,15 +5,32 @@ import { loadCart } from "../data/cart.js";
 //import '../data/cart-class.js';
 //import '../data/backend.js';
 
+async function loadPage() {  // async is basically a shortcut for promise, behind the scenes it returns a promise 
+    
+    //return 'value2' // this is equivalent to the resolve() value
+    await loadProductsFetch();  // we dont have to write a .then(() => {}) function if we are using await, its a basically shortcut for .then()
+                                // we can only use await only inside the async
+    await new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        });
+        });
+    renderOrderSummary();
+    renderPaymentSummary();
+}
+
+loadPage();
+
+
+/*
 Promise.all([       // this creates an array of promises and wait for all the promises to finish at the same time 
                     // this is more efficient, since we dont have to wait for all the promises to finish one by one
 
-    // new Promise((resolve) => {
+    new Promise((resolve) => {
     
-    //     loadProducts(() => {
-    //         resolve('value1'); // we can pass a value to resolve 
-    //     });
-    // }),
+        loadProducts(() => {
+            resolve('value1'); // we can pass a value to resolve 
+        });
     loadProductsFetch(),
     new Promise((resolve) => {
         loadCart(() => {
@@ -26,6 +43,8 @@ Promise.all([       // this creates an array of promises and wait for all the pr
     renderOrderSummary();
     renderPaymentSummary();
 })
+*/
+
 
 /*
 new Promise((resolve) => {
